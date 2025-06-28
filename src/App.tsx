826af088b61f05fc220172393dde7289f209de0e
Sulaimon-ade <107030607+Sulaimon-ade@users.noreply.github.com>
2025-06-28 21:41:26 +0100

    fixed dark mode and give more padding at the top

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
   return (
     <ThemeProvider>
       <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 dark:from-gray-900 dark:to-blue-900 transition-colors duration-300">
-        <div className="container mx-auto px-4 py-8 max-w-7xl">
+        <div className="container mx-auto px-4 pt-12 md:pt-8 pb-8 max-w-7xl">
           {/* Application Header */}
           <Header />
 
